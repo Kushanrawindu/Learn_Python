83664260b772a74bfa97aee3f076d4810f749d28
Kushanrawindu <kushanrawindu@gmail.com>
2020-12-11 13:19:45 +0530

    learn to add 2 numbers from inputs

--- start.py
+++ start.py
@@ -1,9 +1,17 @@
-hello = "kushan"
+# hello = "kushan"
 
-print(10+20)
-print(hello)
+# print(10+20)
+# print(hello)
 
-firstname = input("firstname : ")
-lastname = input("lastname : ")
+# firstname = input("firstname : ")
+# lastname = input("lastname : ")
 
-print("your full name is ",firstname,lastname)
+# print("your full name is ",firstname,lastname)
+
+num1 = input("first number :")
+num2 = input("second number :")
+
+n1 = int(num1)
+n2 = int(num2)
+
+print(n1+n2)
